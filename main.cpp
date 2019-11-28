af6a5d0f65053b64ab9fda8e2865c89935301ce7
billionai <larsen.bruno.p@gmail.com>
2019-11-28 10:30:44 -0300

    changed main.cpp for testing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <stdio.h>
 #include "screenManager.h"
+#include "tableManager.h"
 
 screenManager sm;
 
 int main(){
-	int i = sm.register_new_panel(0,0, 40, 20);
-	sm.mvprint_to_panel(i,1,1,"UF\t|ocupado");
-	sm.mvprint_to_panel(i,2,1,"----------------------");
-	sm.mvprint_to_panel(i,3,1,"ULA\t| X");
+	/*
+	std::vector<std::vector<int> > d;
+	std::string tmp;
+	std::vector<std::string> s;
+	tmp = "Regs|busy";
+	s.push_back(tmp);
+	tmp = "----|----";
+	s.push_back(tmp);
+	tmp = " R0 | no ";
+	s.push_back(tmp);
+	tableManager<int> tm(0,0,s,d);
+	*/
+	std::vector<std::vector<int> >d(1);
+	std::string n = "REGS";
+	std::vector<std::string> col;
+	col.push_back(n);
+	n = "busy";
+	col.push_back(n);
+	std::vector<std::string> lin;
+	n = "R0";
+	lin.push_back(n);
+	n = "R1";
+	lin.push_back(n);
+	tableManager<int> tm(0,0,col,lin,"d",d);
 	getch();
 	return 0;
 }
