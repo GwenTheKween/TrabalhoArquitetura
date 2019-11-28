da1e69bb02f2895f77ee9fe42780e7cd8215596c
cptziltoid <mateus.kastilho@gmail.com>
2019-11-28 11:08:58 -0300

    Implemented RegResultController

--- regResultController.h
+++ regResultController.h
@@ -3,20 +3,28 @@
 
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 class RegResController{
 
 private:
-	//will map register to uf using it
+	//will map register to fu using it
 	std::unordered_map <std::string, std::string> registers;	
 	//the dispatch will have to check availability first so no conflict happens because of the writeback
 public:
-	//precisa definir um valor que permita reconhecer que esta livre
-	
+	RegResController();
+
+	//checks if a register is being used by a fu
 	bool isRegAvailable(std::string regName);
 	
-	void populateReg(std::string regName, std::string ufName);
+	//obtains the functional unit that is using this register
+	std::string getRegister(std::string regName);
 
+	//specifies the functional unit using a register
+	void populateReg(std::string regName, std::string fuName);
+
+	//used when a register is no longer being used
 	void clearReg(std::string regName);
 };
-#endif
+
+#endif // REG_RES_CONTROLLER
