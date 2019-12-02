4438fabf96b55ed251d1422c21c4781107ce6290
billionai <larsen.bruno.p@gmail.com>
2019-12-02 14:56:54 -0300

    changed constructor of regResult, to receive table information

--- regResultController.cpp
+++ regResultController.cpp
@@ -1,8 +1,10 @@
 #include "regResultController.h"
 
+std::vector<std::string> regs = {"F0", "F2", "F4", "F6", "F8", "F10", "F12"};
+
 using namespace std;
-RegResController::RegResController(){
-    vector<string> regs = {"F0", "F2", "F4", "F6", "F8", "F10", "F12"}; //only a certain set of registers, may be expanded
+RegResController::RegResController(tableManager<string>& tm):
+	gui(tm){
     for(int i = 0; i < regs.size(); i++){
         registers.emplace(regs[i], "NULL"); // registers available have a NULL string
     }
