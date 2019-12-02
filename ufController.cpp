c4daf94366cd7cbb532687419670b58c81b31bf5
Vinicius Henrique Borges <viniciusborges22@gmail.com>
2019-12-02 17:31:15 -0300

    UF in production...

--- ufController.cpp
+++ ufController.cpp
@@ -5,29 +5,128 @@ using namespace std;
 
 
 UfController::UfController(){
+	ufLine int1;
+	int1.opName = "Int1";
+	int1.busy = false;
+	ufsInt.push_back(int1);
 
+	ufLine int2;
+	int1.opName = "Int2";
+	int1.busy = false;
+	ufsInt.push_back(int2);
 
+	ufLine float1;
+	int1.opName = "Float1";
+	int1.busy = false;
+	ufsFloat.push_back(float1);
+
+	ufLine float2;
+	int1.opName = "Float2";
+	int1.busy = false;
+	ufsFloat.push_back(float2);
 }
 
-//checks if a compatible fu is available
-bool UfController::hasUfAvailable(bool needsFloatingPointUf){
-
-	return true;
+//checks if a compatible fu is available and return it
+ufLine UfController::hasUfAvailable(bool needsFloatingPointUf){
+	std::vector<ufLine>& ufs = needsFloatingPointUf ? ufsFloat : ufsInt;
+	for(auto uf : ufs)
+	{
+		if(!uf.busy)
+			return uf;
+	}
+	return NULL;
 }
 
-//returns ufName - the name of the chosen fu
-string UfController::populateUf(instruction dispatchedInstruction){
-	return "populateUf";
+void UfController::populateUf(ufLine& uf, instruction& dispatchedInstruction, RegResController& regRes){
+	uf.instructionId = dispatchedInstruction.id;
+	if(uf.opName.substr(0, 3) == "Int")
+		uf.execCyclesLeft = nCyclesFloating[dispatchedInstruction.opName];
+	else
+		uf.execCyclesLeft = 1;
+	uf.next_busy = true;
+	uf.opName = dispatchedInstruction.opName;
+	if(dispatchedInstruction.isRtype)
+	{
+		uf.fi = dispatchedInstruction.rd;
+		uf.fj = dispatchedInstruction.rs;
+		uf.fk = dispatchedInstruction.rt;
+	}
+	else
+	{
+		if(dispatchedInstruction.opName != "SW")
+		{
+			uf.fi = dispatchedInstruction.rt;
+			uf.fj = dispatchedInstruction.rs;
+		}
+		else
+		{
+			uf.fi = "-";
+			uf.fj = dispatchedInstruction.rs;
+			uf.fk = dispatchedInstruction.rt;
+		}
+	}
+	uf.qj = regRes.isRegAvailable(uf.fj) ? "0" : regRes.getRegister(uf.fj);
+	uf.qk = regRes.isRegAvailable(uf.fk) ? "0" : regRes.getRegister(uf.fk);
+
+	uf.rj = uf.qj == "0" ? 1 : 0;
+	uf.rk = uf.qk == "0" ? 1 : 0;
 }
 
 //returns false if operands not ready otherwise returns true
 bool UfController::readOperands(int instructionId, string opName){ //alguma ideia melhor? precisa identificar a instrucao de alguma forma... talvez so id
-	return true;
-
+	for(auto uf : ufsInt)
+	{
+		if(uf.instructionId == instructionId)
+		{
+			if(uf.rj && uf.rk)
+			{
+				uf.rj = uf.rk = 0;
+				uf.qj = uf.qk = "0";
+				return true;
+			}
+			else
+				return false;
+		}
+	}
+	for(auto uf : ufsFloat)
+	{
+		if(uf.instructionId == instructionId)
+		{
+			if(uf.rj && uf.rk)
+			{
+				uf.rj = uf.rk = 0;
+				uf.qj = uf.qk = "0";
+				return true;
+			}
+			else
+				return false;
+		}
+	}
+	fprintf(stderr, "Erro em readOperands: instrucao nao encontrada na UF");
+	return false;
 }
+
 //returns false if UF not yet done otherwise returns true
 bool UfController::runExecution(int instructionId, string opName){
-	return true;
+	for(auto uf : ufsInt)
+	{
+		if(uf.instructionId == instructionId)
+		{
+			uf.execCyclesLeft--;
+			return true;
+		}
+	}
+	for(auto uf : ufsFloat)
+	{
+		if(uf.instructionId == instructionId)
+		{
+			uf.execCyclesLeft--;
+			if(uf.execCyclesLeft == 0)
+				return true;
+			break;
+		}
+	}
+	return false;
 }
 
 //returns false if the informed register is still waiting to be read by a UF otherwise returns true
